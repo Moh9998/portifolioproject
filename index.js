9feb1fc66a4ca35b6e9bcd0e8162086f4427a3ed
Moh9998 <68030846+Moh9998@users.noreply.github.com>
2022-09-06 20:24:42 +0300

    finallizing-the-mobile-menu

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-// const menu= document.getElementById('#burger')
-// const list= document.getElementById('#nav-list');
-// const closeIcon=document.getElementsByClassName('.close-icon')
 
-function open(){
-document.getElementById('burger')
-clsslist.toggle('.close-img ')
+
+function menu(){
+  let nav= document.getElementById("nav-header")  
+    nav.style.display="flex"
+    let icon= document.getElementById('close-icon')
+    let burger=document.getElementById('burger')
+    burger.style.display="none"
+    icon.style.display="inline-block"
+    document.getElementById('img-deco1').style.display="block"
+    document.getElementById('img-dots').style.display="block"
+    document.getElementById('img-deco2').style.display="block"
+
 }
 
+function closeMenu(){
+    let nav= document.getElementById("nav-header")  
+    nav.style.display="none"
+    let icon= document.getElementById('close-icon')
+    let burger=document.getElementById('burger')
+    burger.style.display="flex"
+    icon.style.display="none"
+    document.getElementById('img-deco1').style.display="none"
+    document.getElementById('img-dots').style.display="none"
+    document.getElementById('img-deco2').style.display="none"
+
+}
