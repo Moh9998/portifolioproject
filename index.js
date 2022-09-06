c5713d2239a48cee69e82d2de39d184ae78bfd7a
Moh9998 <68030846+Moh9998@users.noreply.github.com>
2022-09-06 15:18:38 +0300

    making-the-meobile-menu

---
+++ index.js
@@ -0,0 +1,26 @@
+function grab(e) {
+    return document.getElementById(e);
+  }
+  
+  const menu = document.getElementById('burger');
+  const options = document.querySelectorAll('.option');
+  
+  menu.addEventListener('click', () => {
+    grab('open').style.display = 'none';
+    grab('closed').style.display = 'block';
+    grab('nav--mobile').style.display = 'block';
+  });
+  
+  for (let i = 0; i < options.length; i += 1) {
+    options[i].addEventListener('click', () => {
+      grab('open').style.display = 'block';
+      grab('closed').style.display = 'none';
+      grab('nav--mobile').style.display = 'none';
+    });
+  }
+  
+  grab('closed').addEventListener('click', () => {
+    grab('open').style.display = 'block';
+    grab('closed').style.display = 'none';
+    grab('nav--mobile').style.display = 'none';
+  });
