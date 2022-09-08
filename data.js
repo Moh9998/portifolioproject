c2674cba1c2535a73ccc9c49fea4cbc6bc817540
KareemWilson <kareemmagdywilson@gmail.com>
2022-09-08 16:52:59 +0200

    Create object of data

---
+++ data.js
@@ -0,0 +1,52 @@
+const projectData = [
+  {
+    id: "1",
+    name: "project 1",
+    img: "",
+    description:
+      "Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exm ipsum dolor sit amet consectetur adipisicing elit. Nobis ad asperiores facilis sequi maiores, ducimus saepe placeat possimus porro numquam molestiae id quis similique quas corrupti, eaque sit nihil aut.",
+    technologies: ["javascript", "html", "ruby", "css"],
+    linkToLive: "",
+    sourceLink: "",
+  },
+  {
+    id: "1",
+    name: "project 1",
+    img: "",
+    description:
+      "Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exm ipsum dolor sit amet consectetur adipisicing elit. Nobis ad asperiores facilis sequi maiores, ducimus saepe placeat possimus porro numquam molestiae id quis similique quas corrupti, eaque sit nihil aut.",
+    technologies: ["javascript", "html", "ruby", "css"],
+    linkToLive: "",
+    sourceLink: "",
+  },
+  {
+    id: "1",
+    name: "project 1",
+    img: "",
+    description:
+      "Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exm ipsum dolor sit amet consectetur adipisicing elit. Nobis ad asperiores facilis sequi maiores, ducimus saepe placeat possimus porro numquam molestiae id quis similique quas corrupti, eaque sit nihil aut.",
+    technologies: ["javascript", "html", "ruby", "css"],
+    linkToLive: "",
+    sourceLink: "",
+  },
+  {
+    id: "1",
+    name: "project 1",
+    img: "",
+    description:
+      "Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exm ipsum dolor sit amet consectetur adipisicing elit. Nobis ad asperiores facilis sequi maiores, ducimus saepe placeat possimus porro numquam molestiae id quis similique quas corrupti, eaque sit nihil aut.",
+    technologies: ["javascript", "html", "ruby", "css"],
+    linkToLive: "",
+    sourceLink: "",
+  },
+  {
+    id: "1",
+    name: "project 1",
+    img: "",
+    description:
+      "Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exm ipsum dolor sit amet consectetur adipisicing elit. Nobis ad asperiores facilis sequi maiores, ducimus saepe placeat possimus porro numquam molestiae id quis similique quas corrupti, eaque sit nihil aut.",
+    technologies: ["javascript", "html", "ruby", "css"],
+    linkToLive: "",
+    sourceLink: "",
+  },
+];
