11e28ee842ceaa683d5f2271d6b0f03a4cc99b17
Moh9998 <68030846+Moh9998@users.noreply.github.com>
2022-09-13 15:20:51 +0300

    making-changes

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-
-
-function menu(){
-  let nav= document.getElementById("nav-header")  
-    nav.style.display="flex"
-    let icon= document.getElementById('close-icon')
-    let burger=document.getElementById('burger')
-    burger.style.display="none"
-    icon.style.display="inline-block"
-    document.getElementById('img-deco1').style.display="block"
-    document.getElementById('img-dots').style.display="block"
-    document.getElementById('img-deco2').style.display="block"
-
+function menu() {
+  const nav = document.getElementById('nav-header');
+  nav.style.display = 'flex';
+  const icon = document.getElementById('close-icon');
+  const burger = document.getElementById('burger');
+  burger.style.display = 'none';
+  icon.style.display = 'inline-block';
+  document.getElementById('img-deco1').style.display = 'block';
+  document.getElementById('img-dots').style.display = 'block';
+  document.getElementById('img-deco2').style.display = 'block';
 }
 
-function closeMenu(){
-    let nav= document.getElementById("nav-header")  
-    nav.style.display="none"
-    let icon= document.getElementById('close-icon')
-    let burger=document.getElementById('burger')
-    burger.style.display="flex"
-    icon.style.display="none"
-    document.getElementById('img-deco1').style.display="none"
-    document.getElementById('img-dots').style.display="none"
-    document.getElementById('img-deco2').style.display="none"
-
+function closeMenu() {
+  const nav = document.getElementById('nav-header');
+  nav.style.display = 'none';
+  const icon = document.getElementById('close-icon');
+  const burger = document.getElementById('burger');
+  burger.style.display = 'flex';
+  icon.style.display = 'none';
+  document.getElementById('img-deco1').style.display = 'none';
+  document.getElementById('img-dots').style.display = 'none';
+  document.getElementById('img-deco2').style.display = 'none';
 }
